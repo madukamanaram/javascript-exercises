56e44f8f35d8040163514acdd29fbee7bdd92e03
madukamanaram <madukamanaram@gmail.com>
2026-07-23 22:45:01 +0530

    07_reverseString/08_removeFromArray

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let newStr = "";
+    let charCount = str.length;
+    
+    for (let index = charCount -1  ;0 <= index ; index--) {
+        let element = str[index];
+        newStr += element;
 
+    }
+    return newStr;
 };
+
+console.log(reverseString("hello"))
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,39 @@
-const removeFromArray = function() {
-};
+ const removeFromArray = function(arr, ...args) {
+    // let newArr = [];
+    
+    // for(let item1 of arr){
+    //     let shouldNotRemove = true;
+
+    //     for (let item2 of args){
+    //         if(item1 === item2){
+    //             shouldNotRemove = false;
+    //         }
+    //         break;
+    //     }
+        
+    
+    // if(shouldNotRemove){
+    //     newArr.push(item1)
+
+    // }
+    // }
+    // return newArr
+    
+    // ---------------------------------------------------------------
+    
+    
+    
+    let newArr = [];
+    arr.forEach(element => {
+        if(!args.includes(element)) newArr.push(element)
+        });
+        return newArr
+
+
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
